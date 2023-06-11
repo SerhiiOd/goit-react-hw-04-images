86e5cd9bff4f5f66b135614a5e0d28d56383ea9e
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-06-12 00:42:31 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 import Searchbar from 'components/Searchbar/Searchbar';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
@@ -8,41 +8,24 @@ import { fetchImages } from 'services/imageApi';
 import { Button } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 
-export class App extends Component {
-  state = {
-    searchValue: '',
-    images: null,
-    pageNumber: 1,
-    loadMore: false,
-    isLoading: false,
-  };
+export default function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [images, setImages] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loadMore, setLoadMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchValue !== this.state.searchValue ||
-      prevState.pageNumber !== this.state.pageNumber
-    ) {
-      this.fetchGallery();
-    }
-  }
-
-  fetchGallery = async () => {
+  const fetchGallery = async () => {
     try {
-      const { hits, totalHits } = await fetchImages(
-        this.state.pageNumber,
-        this.state.searchValue
-      );
+      const { hits, totalHits } = await fetchImages(pageNumber, searchValue);
 
       if (hits.length === 0) {
         toast.error("Sorry, images not found... But you can try: 'Apple'");
         return;
       }
 
-      this.setState(prevState => ({
-        images: [...prevState.images, ...hits],
-        loadMore: this.state.pageNumber < Math.ceil(totalHits / 12),
-        isLoading: false,
-      }));
+      setImages(prevImages => [...prevImages, ...hits]);
+      setLoadMore(pageNumber < Math.ceil(totalHits / 12));
 
       if (totalHits <= 12) {
         toast("Oh, but that's all?! But you can try: 'Car'");
@@ -50,39 +33,38 @@ export class App extends Component {
     } catch (error) {
       console.log(error);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  onLoadMore = () => {
-    this.setState(prevState => ({
-      pageNumber: prevState.pageNumber + 1,
-    }));
+  useEffect(() => {
+    if (searchValue !== '') {
+      fetchGallery();
+    }
+  }, [searchValue, pageNumber]);
+
+  const onLoadMore = () => {
+    setIsLoading(true);
+    setPageNumber(prevPageNumber => prevPageNumber + 1);
   };
 
-  onSearchSubmit = searchValue => {
-    this.setState({
-      searchValue,
-      images: [],
-      pageNumber: 1,
-      loadMore: false,
-      isLoading: true,
-    });
+  const onSearchSubmit = searchValue => {
+    setIsLoading(true);
+    setSearchValue(searchValue);
+    setImages([]);
+    setPageNumber(1);
+    setLoadMore(false);
   };
 
-  render() {
-    const { images, isLoading, loadMore } = this.state;
+  return (
+    <BoxDiv>
+      <Searchbar onSubmit={onSearchSubmit} />
+      {images.length > 0 && <ImageGallery images={images} />}
 
-    return (
-      <BoxDiv>
-        <Searchbar onSubmit={this.onSearchSubmit} />
-        <ImageGallery images={images} />
+      {isLoading && <Loader />}
+      {loadMore && <Button onClick={onLoadMore} />}
 
-        {isLoading && <Loader />}
-        {loadMore && <Button onClick={this.onLoadMore} />}
-
-        <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
-      </BoxDiv>
-    );
-  }
+      <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
+    </BoxDiv>
+  );
 }
